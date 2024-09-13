a861634df0962f96009d128dd807243ac74c15c3
srinu <srinivasveligethi@gmail.com>
2024-09-14 01:12:03 +0530

    stage_01 completed

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from kidneyTumorDetection import logger
+from kidneyTumorDetection.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info("Hello world")
+STAGE_NAME = "Data Ingestion stage"
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx======x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
