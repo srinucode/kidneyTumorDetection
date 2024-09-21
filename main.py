991d1c1ef41de1d2c91d781df1143f3da8ea6baf
srinu <srinivasveligethi@gmail.com>
2024-09-21 15:03:23 +0530

    Training stage completed

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
 from kidneyTumorDetection import logger
 from kidneyTumorDetection.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from kidneyTumorDetection.pipeline.stage02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from kidneyTumorDetection.pipeline.stage03_model_training import ModelTrainingPipeline
+
 
 
 STAGE_NAME = "Data Ingestion stage"
-if __name__ == '__main__':
-    try:
-        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
-        obj = DataIngestionTrainingPipeline()
-        obj.main()
-        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx======x")
-    except Exception as e:
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
         logger.exception(e)
         raise e
 
 
 STAGE_NAME = "Prepare base model"
-if __name__ == '__main__':
-    try:
-        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
-        obj = PrepareBaseModelTrainingPipeline()
-        obj.main()
-        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx======x")
-    except Exception as e:
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
         logger.exception(e)
         raise e
 
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
